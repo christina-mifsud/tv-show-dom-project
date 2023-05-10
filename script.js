a4dfd57dd8522ad2995ac6b4d70a7599ab15e23e
christina-mifsud <christina.mifsud@gmail.com>
2023-05-10 20:42:27 +0100

    Level 300
    WIP

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//You can edit ALL of the code here
 const allEpisodes = getAllEpisodes();
 
 function setup() {
@@ -9,39 +8,62 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  // level 100
   document.getElementById("root").innerHTML = "";
   for (i = 0; i < episodeList.length; i++) {
-    // create elements
+    // step 1 - create elements
     let episodeDivContainer = document.createElement("div");
     let episodeTitleElement = document.createElement("h3");
     let episodeImageElement = document.createElement("img");
     let episodeBlurbElement = document.createElement("p");
 
     // assign the divs classes
-    episodeDivContainer.classList = "episode-container";
-    episodeTitleElement.classList = "episode-title";
-    episodeImageElement.classList = "episode-image";
-    episodeBlurbElement.classList = "episode-blurb";
+    episodeDivContainer.classList.add("episode-container");
+    episodeTitleElement.classList.add("episode-title");
+    episodeImageElement.classList.add("episode-image");
+    episodeBlurbElement.classList.add("episode-blurb");
 
-    // contents of elements
+    // step 2 - contents of elements
     episodeTitleElement.innerHTML = `${episodeList[i].name}-S${String(
       episodeList[i].season
     ).padStart(2, "0")}E${String(episodeList[i].number).padStart(2, "0")}`;
     episodeImageElement.src = `${episodeList[i].image.medium}`;
     episodeBlurbElement.innerHTML = `${episodeList[i].summary}`;
 
-    // append elements to root
+    // step 3 - append elements to root
     document.getElementById("root").appendChild(episodeDivContainer);
     episodeDivContainer.appendChild(episodeTitleElement);
     episodeDivContainer.appendChild(episodeImageElement);
     episodeDivContainer.appendChild(episodeBlurbElement);
   }
+
+  // level 300 - episode selector
+
+  let dropdown = document.getElementById("episode-select");
+
+  let defaultOption = document.createElement("option"); // step 1 createElement
+  defaultOption.textContent = "Select Episode"; // step 2 set textContent
+  dropdown.appendChild(defaultOption); // step 3 appendChild
+  dropdown.selectedIndex = 0;
+
+  for (i = 0; i < episodeList.length; i++) {
+    let option = document.createElement("option");
+    option.value = episodeList[i].name;
+    option.textContent = `S${episodeList[i].season
+      .toString()
+      .padStart(2, "0")}E${episodeList[i].number
+      .toString()
+      .padStart(2, "0")} - ${episodeList[i].name}`;
+    dropdown.appendChild(option);
+  }
 }
 
 window.onload = setup;
-makePageForEpisodes(allEpisodes);
+makePageForEpisodes(allEpisodes); // OG
 
-// search bar
+// level 200 - search bar
+
 document
   .getElementById("search-input")
   .addEventListener("input", searchEpisode);
@@ -64,3 +86,7 @@ function searchEpisode() {
 
   // BUG - will search for matches even in they are part of a longer word (eg. 'die' will also show 'soldier')
 }
+
+///// window.location.href = option.value = `${episode.url}` -> to open episode URL
+
+//////////////////////////////////////////
